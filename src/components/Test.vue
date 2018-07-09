6e1f7f732e3a2593d2817a6031fb4e9d4d4846ea
SYJACK7 <529704977@qq.com>
2018-07-09 17:00:51 +0800

    Test
    2018.7.9

---
+++ src/components/Test.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="hello">
+    <h1>This is a Vue project template</h1>
+
+  </div>
+</template>
+<script>
+  export default {
+    name: 'Test',
+    data:{
+        treeData:[]
+    }
+  }
+</script>
